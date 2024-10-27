91a5a6b87d0012b1f20002fa0eddba853705a411
skaytskayt <k.a.lopaev@gmail.com>
2024-10-27 14:17:42 +0300

    added symbol out put

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
 const text = "Привет, я калькулятор!"
+let text1 =''
+
+let cursor = document.createElement('span')
+cursor.className = 'cursor'
+cursor.textContent = "|"
+
+
+let index = 0
 
 const input = document.querySelector(".input-task")
 
 
-function setPlaceholder(text){
-  input.placeholder = text
+const p = document.querySelector('.greetings')
+
+
+function symbolOutPut(){
+  if (index < text.length){
+    text1 += text[index]
+    p.textContent = text1
+    p.append(cursor)
+    index ++
+    setTimeout(symbolOutPut, 100)
+  }
 }
 
-setPlaceholder(text)
+symbolOutPut()
+
 
 
 
@@ -15,13 +33,26 @@ setPlaceholder(text)
 
 
 document.addEventListener("keydown", (event) => {
+
   if (event.key === 'Enter'){
     event.preventDefault()
     calculate(input.value)
   }
 
-  if (event.key >= '0' && event.key <= '9' || event.key === '+' || event.key === '-' || event.key === '*' || event.key === '/' || event.key === '.'){
+  if (event.key >= '0' && event.key <= '9'){
+    let preDisplay = input.value
+    let lastOperIndex = findLastOperatorIndex(preDisplay)
+    let numbPart = preDisplay.slice(lastOperIndex + 1).split(' ').join('')
+    numbPart += event.key
+    numbPart = String(Number(numbPart).toLocaleString('en').replaceAll(',', ' '))
+    input.value = preDisplay.slice(0, lastOperIndex + 1) + numbPart
+
+  }
+  
+
+  if (event.key === '+' || event.key === '-' || event.key === '*' || event.key === '/' || event.key === '.'){
     input.value += event.key
+    
   }
 
   if (event.key === 'Backspace'){
@@ -32,14 +63,46 @@ document.addEventListener("keydown", (event) => {
 
 
 
+
+function findLastOperatorIndex(str){
+  let operators = ['+', '-', '*', '/', '.']
+  let lastOperIndex = -1
+  operators.forEach((char) => {
+    let index = str.lastIndexOf(char)
+    if (index > lastOperIndex){
+      lastOperIndex = index
+    }
+  })
+  return lastOperIndex
+}
+
+
+
+
 function addOnDisplay(inputNumb){
-  input.value += inputNumb
+  if (inputNumb >= '0' && inputNumb <= '9'){
+    let preDisplay = input.value
+    let lastOperIndex = findLastOperatorIndex(preDisplay)
+    let numbPart = preDisplay.slice(lastOperIndex + 1).split(' ').join('')
+    numbPart += inputNumb
+    numbPart = String(Number(numbPart).toLocaleString('en').replaceAll(',', ' '))
+    input.value = preDisplay.slice(0, lastOperIndex + 1) + numbPart
+  }
+
+  if (['+', '-', '*', '/', '.'].includes(inputNumb)){
+    input.value += inputNumb
+  }
+
 }
 
 function removeOnDisplay(){
-  input.value = input.value.slice(0, -1)
+  input.value = input.value.trim().slice(0, -1)
 }
 
 function calculate(inputExample){
-  input.value = eval(inputExample)
+  let countInput = inputExample.split(' ').join('')
+  input.value = eval(countInput).toLocaleString('en').replaceAll(',', ' ')
 }
+
+
+
